53b83066bb60815e807c9dfe2cb35ac535f5bafc
Patrick <patrickmcmanamon101@gmail.com>
2025-09-22 21:12:26 +0100

    Initial Commit 2

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,11 +1,7 @@
 package client
 
 import (
-	"context"
-	"golang.org/x/oauth2/clientcredentials"
-	"log"
 	"net/http"
-	"os"
 )
 
 //go:generate oapi-codegen -config ../../cfg.yaml ../../cloudapi.yaml
@@ -14,26 +10,4 @@ const BASE_URL = "https://fidelis.isams.cloud"
 
 type APIClient struct {
 	*http.Client
-}
-
-func GetHTTPClient() APIClient {
-	clientSecret := os.Getenv("ISAMS_REST_CLIENT_SECRET")
-
-	if clientSecret == "" {
-		log.Fatalln("Couldn't find ISAMS_REST_CLIENT_SECRET env var")
-	}
-
-	ctx := context.Background()
-	conf := clientcredentials.Config{
-		ClientID:       "982A09D3-E738-4597-8270-310593223716",
-		ClientSecret:   clientSecret,
-		TokenURL:       BASE_URL + "/auth/connect/token",
-		Scopes:         []string{"restapi"},
-		EndpointParams: nil,
-		AuthStyle:      0,
-	}
-
-	c := conf.Client(ctx)
-
-	return APIClient{c}
 }
--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 type responseWriter struct {
@@ -14,30 +13,6 @@ type responseWriter struct {
 func (rw *responseWriter) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
-}
-
-func Logger(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		// Create a custom ResponseWriter to capture the status code
-		wrapped := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-		// Call the next handler in the chain
-		next.ServeHTTP(wrapped, r)
-
-		// Log after the request is handled
-		duration := time.Since(start)
-		if wrapped.statusCode < 400 || wrapped.statusCode == 404 {
-			log.Printf(
-				"%s %s %d %s",
-				r.Method,
-				r.URL.Path,
-				wrapped.statusCode,
-				duration,
-			)
-		}
-	}
 }
 
 func LogAndError(r *http.Request, w http.ResponseWriter, friendlyErrMsg string, errMsg string, code int) {

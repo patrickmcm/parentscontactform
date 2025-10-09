af8d86cc934afcf38b4997ea0d8161c3100970c7
Patrick <patrickmcmanamon101@gmail.com>
2025-10-09 17:32:29 +0100

    Added sentry logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"github.com/getsentry/sentry-go"
+	sentryhttp "github.com/getsentry/sentry-go/http"
 	"io/fs"
 	"log"
 	"net/http"
@@ -28,6 +31,21 @@ func main() {
 		log.Fatalf("Failed to setup OIDC: %v", err)
 	}
 
+	if err = sentry.Init(sentry.ClientOptions{
+		Dsn: "https://ea3d5a69736ab2a9f530ab0d28a4a9fa@o4510160253747200.ingest.de.sentry.io/4510160288219216",
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for tracing.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+		// Enable structured logs to Sentry
+		EnableLogs:    true,
+		EnableTracing: true,
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
+	sentryHandler := sentryhttp.New(sentryhttp.Options{})
+
 	hc := client.GetHTTPClient()
 	c, err := client.NewClientWithResponses(client.BASE_URL, client.WithHTTPClient(&hc))
 	if err != nil {
@@ -39,14 +57,14 @@ func main() {
 	staticSubFS, _ := fs.Sub(staticFS, "static")
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.FS(staticSubFS))))
 
-	http.HandleFunc("/", handler.HandleLoginGet)
-	http.HandleFunc("/login", handler.HandleLoginPost)
-	http.HandleFunc("/logout", handler.HandleLogoutGet)
-	http.HandleFunc("/form", handler.HandleFormGet)
-	http.HandleFunc("/children", handler.HandleChildFormGet)
-	http.HandleFunc("/updateChildren", handler.HandleChildFormPost)
-	http.HandleFunc("/callback", handler.HandleCallback)
-	http.HandleFunc("/submit", handler.HandleFormPost)
+	http.HandleFunc("/", sentryHandler.HandleFunc(handler.HandleLoginGet))
+	http.HandleFunc("/login", sentryHandler.HandleFunc(handler.HandleLoginPost))
+	http.HandleFunc("/logout", sentryHandler.HandleFunc(handler.HandleLogoutGet))
+	http.HandleFunc("/form", sentryHandler.HandleFunc(handler.HandleFormGet))
+	http.HandleFunc("/children", sentryHandler.HandleFunc(handler.HandleChildFormGet))
+	http.HandleFunc("/updateChildren", sentryHandler.HandleFunc(handler.HandleChildFormPost))
+	http.HandleFunc("/callback", sentryHandler.HandleFunc(handler.HandleCallback))
+	http.HandleFunc("/submit", sentryHandler.HandleFunc(handler.HandleFormPost))
 
 	err = http.ListenAndServe(os.Getenv("PORT"), nil)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.24.3
 
 require (
 	github.com/coreos/go-oidc v2.4.0+incompatible
+	github.com/getsentry/sentry-go v0.35.3
 	github.com/oapi-codegen/runtime v1.1.2
 	golang.org/x/oauth2 v0.31.0
 )
@@ -16,5 +17,7 @@ require (
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/pquerna/cachecontrol v0.2.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
 )
